e406de8f862d04e9d4b6eccac8b9b12046298fc4
Thomas Kelly <tmkelly28@gmail.com>
2016-03-24 19:04:21 -0400

    Initial

--- app/index.js
+++ app/index.js
@@ -1,3 +1,39 @@
 'use strict';
 
-console.log('Hello ES6!');
+const readline = require('readline');
+const http = require('http');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function main () {
+
+  console.log('Welcome to ConcertGoer');
+
+  rl.question('What do you want to do? > ', command => {
+    switch (command) {
+      case 'search':
+        rl.question('Enter an artist to search for > ', bandName => {
+          let responseBody = '';
+          http.get({
+            host: 'api.bandsintown.com',
+            path: '/artists/' + bandName.replace(/\s/g, '%20') + '/events.json?app_id=ES6_LECTURE'
+          }, res => {
+            res.on('data', data => responseBody += data);
+            res.on('end', () => {
+              console.log(JSON.parse(responseBody));
+            })
+          })
+          .on('error', error => console.error);
+        });
+        break;
+      default:
+        console.log('Not a valid command');
+        main();
+    }
+  });
+}
+
+main();
